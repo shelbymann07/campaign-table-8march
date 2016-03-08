e92999ec8748aa21b73b5cd92b70c67bedf2e5be
Chris Canipe <chris.canipe@wsj.com>
2016-03-08 17:37:08 -0600

    prepping project to play with libraries in class

--- js/script.js
+++ js/script.js
@@ -1,18 +1,21 @@
-
-// Assignment:
-// Create a list of all of the donations to Kurt Schaefer's Campaign
-// (CITIZENS TO ELECT KURT SCHAEFER ATTORNEY GENERAL)
-
-
 $(document).ready(function() {
 
 	// 1.
 	// When the page is loaded,
 	// call the loadData() function.
 
+	loadData();
+
 });
 
+
+
+
 function loadData() {
+	
+	$.getJSON("js/donations_over_5K.json", function(data) {
+		writeTable(data);
+	});
 
 	// 2.
 	// Write an AJAX call here to load your data.
@@ -20,7 +23,27 @@ function loadData() {
 }
 
 
+
 function writeTable(data) {
+
+		for(i=0; i < data.length; i++) {
+
+			if (data[i]["Committee"] === "CITIZENS TO ELECT KURT SCHAEFER ATTORNEY GENERAL") {
+				$("table.donations tbody").append(
+					"<tr>"+
+						"<td class='date'>"+data[i]["Contribution Date"]+"</td>"+
+		                "<td class='info'>"+data[i]["Contribution Information"]+"</td>"+
+		                "<td class='amount'>"+data[i]["Amount"]+"</td>"+
+	                "</tr>"
+				);
+			}
+
+		}
+
+
+		$("table.donations").dataTable();
+
+
 
 	// 3.
 	// Make a list of every donation made to "CITIZENS TO ELECT KURT SCHAEFER ATTORNEY GENERAL"
