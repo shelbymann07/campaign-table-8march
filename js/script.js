d84f0ce46150d04d5ec38cede2afbfa2107a74a6
Shelby Mann <smann@mwc-021178.dhcp.missouri.edu>
2016-03-08 19:40:52 -0600

    first commit of in class work

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
+var months = {
+    "01" : "January",
+    "02" : "February",
+    "03" : "March",
+    "04" : "April",
+    "05" : "May",
+    "06" : "June",
+    "07" : "July",
+    "08" : "August",
+    "09" : "September",
+    "10" : "October",
+    "11" : "November",
+    "12" : "December"
+}
+
 $(document).ready(function() {
 
 	// 1.
@@ -27,20 +42,34 @@ function loadData() {
 function writeTable(data) {
 
 		for(i=0; i < data.length; i++) {
+            
+            var longDate = data[i]["Contribution Date"];
+            var dateAsArray = longDate.split("-");
+            
+            var monthNum = dateAsArray[1];
+            
+            var verboseDate = moment(longDate).format('MMM. D, YYYY');
+            
+            //console.log(verboseDate);
 
-			if (data[i]["Committee"] === "CITIZENS TO ELECT KURT SCHAEFER ATTORNEY GENERAL") {
+			//if (data[i]["Committee"] === "CITIZENS TO ELECT KURT SCHAEFER ATTORNEY GENERAL") {
 				$("table.donations tbody").append(
 					"<tr>"+
-						"<td class='date'>"+data[i]["Contribution Date"]+"</td>"+
+						"<td data-sort='"+longDate+"' class='date'>"+verboseDate+"</td>"+
+                        "<td class='committee'>"+data[i]["Committee"]+"</td>"+
 		                "<td class='information'>"+data[i]["Contribution Information"]+"</td>"+
 		                "<td class='amount'>"+data[i]["Amount"]+"</td>"+
 	                "</tr>"
 				);
-			}
+			//} 
 
 		}
-
-
+    
+    $('.donations').DataTable( 
+        {
+            "order": [[ 3, "desc" ]]
+        } 
+    );
 
 
 
